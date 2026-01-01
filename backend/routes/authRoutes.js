45b9c73bb14db9a47a54785ef557459291f4f118
Bigyanhub <bigyanbasnet849@gmail.com>
2026-01-01 21:33:30 +0545

    feat: implement authentication routes and middleware integration

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { body } from "express-validator";
+import {
+  register,
+  login,
+  getProfile,
+  updateProfile,
+  changePassword,
+} from "../controllers/authController.js";
+import protect from "../middleware/auth.js";
+
+const router = express.Router();
+
+//Validate middleware
+const registerValidation = [
+  body("username")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Username must be least 3 characters"),
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Please provide a valid email"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+];
